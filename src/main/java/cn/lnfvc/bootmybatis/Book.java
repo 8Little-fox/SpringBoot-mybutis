837e52bf5e66fe4f4ba8cb744f51e6b38b9f9856
hcwd <3462540450@qq.com>
2019-12-30 08:59:22 +0800

    SpringBoot_mybutis

---
+++ src/main/java/cn/lnfvc/bootmybatis/Book.java
@@ -0,0 +1,23 @@
+package cn.lnfvc.bootmybatis;
+
+public class Book {
+    private Integer id;
+    private String uname;
+
+    public String getUname() {
+        return uname;
+    }
+
+    public void setUname(String uname) {
+        this.uname = uname;
+    }
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+
+}
